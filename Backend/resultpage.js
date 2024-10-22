1996e747a7af988d6e9e3f4b0cf8272683a78040
RAHULGIT99 <rahulvalavoju123@gmail.com>
2024-10-22 13:08:02 +0530

    The OTP verfication code is working perfectly and all of the issues related to sign in,sign up and verification has been resolved through this commit the backend part is done but if possible I'll do forgot password in the future....The original front end code done by other team member needs to be integrated

--- Backend/resultpage.js
+++ Backend/resultpage.js
@@ -2,22 +2,22 @@
 var mongoose = require("mongoose")
 var bodyparse = require('body-parser')
 var cors = require('cors');
-// const otpGenerator = require('otp-generator');
-// const nodemailer = require('nodemailer');
+const otpGenerator = require('otp-generator');
+const nodemailer = require('nodemailer');
 var exp = require('express');
 var app = exp()
 app.use(cors())
 app.use(exp.json())
-async function mongo(){
+async function mongo() {
     const conn = await mongoose.connect("mongodb://localhost:27017/")
 }
 mongo();
 //model is a way in which we will design our schema
 const TodoSchema = new mongoose.Schema({
-    Name: {type: String, required: true},
-    Age: {type: Number, required: true},
-    Email: {type: String, required: true},
-    Password: {type: String, required: true}
+    Name: { type: String, required: true },
+    Age: { type: Number, required: true },
+    Email: { type: String, required: true },
+    Password: { type: String, required: true }
 });
 const Todo = mongoose.model('Todo', TodoSchema);
 const otpSchema = new mongoose.Schema({
@@ -35,71 +35,161 @@ app.get("/signin", function (req, res) {
 app.post("/signin", async (req, res) => {
     var reqbody = req.body
     console.log(reqbody)
-    var response_value={}
+    var response_value = {}
     var k2 = await Todo.findOne({ Password: reqbody.password });
     var k1 = await Todo.findOne({ Email: reqbody.email });
     var k = await Todo.findOne({ Email: reqbody.email, Password: reqbody.password });
     console.log(k)
-    if(k1==null){
+    if (k1 == null) {
         response_value = {
-            "reply" : "false",
-            "to_be_displayed" : "Please enter correct email"
+            "reply": "false",
+            "to_be_displayed": "Please enter correct email"
         }
-        
     }
-    else if(k1!=null && k2==null){
+    else if (k1 != null && k2 == null) {
         response_value = {
-            "reply" : "false",
-            "to_be_displayed" : "Please enter correct password for your email"
+            "reply": "false",
+            "to_be_displayed": "Please enter correct password for your email"
         }
-    }   
-    else{
-        if(!k1.equals(k2)){
+    }
+    else {
+        if (!k1.equals(k2)) {
             response_value = {
-                "reply" : "false",
-                "to_be_displayed" : "Please enter correct password for your email"
+                "reply": "false",
+                "to_be_displayed": "Please enter correct password for your email"
             }
         }
-        else{
+        else {
             response_value = {
-                "reply" : "true",
-                "to_be_displayed" : "Logged in successfully"
+                "reply": "true",
+                "to_be_displayed": "Logged in successfully"
             }
         }
     }
     console.log(response_value)
     res.json(response_value)
 })
-app.post("/signup", async (req, res) => {
-    try {
-        var resu="false"
-        console.log(req.body)
-        const todo = new Todo(req.body)
-        console.log(todo)
-        var response_value = {}
-        var k1 = await Todo.findOne({ Email: req.body.Email });
-        if(k1==null){
-            await todo.save() 
-            response_value = {
-                "reply" : "true",
-                "to_be_displayed" : "Siggned up successfully"
-            }
+// app.post("/signup", async (req, res) => {
+//     try {
+//         console.log(req.body)
+//         var complete_info_singnup = {
+//             "Name" : req.body.Name,
+//             "Age" : req.body.Age ,
+//             "Email" : req.body.Email,
+//             "Password" : req.body.Password,
+//           }
+//         const todo = new Todo(complete_info_singnup)
+//         console.log(todo)
+//         var response_value = {}
+//         var k1 = await Todo.findOne({ Email: req.body.Email });
+//         if (k1 == null) {
+//             await todo.save()
+//             response_value = {
+//                 "reply": "true",
+//                 "to_be_displayed": "Siggned up successfully"
+//             }
+//         }
+//         else {
+//             response_value = {
+//                 "reply": "false",
+//                 "to_be_displayed": "Please enter another email for registering"
+//             }
+//         }
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+//     finally {
+//         res.json(response_value)
+//     }
+
+// })
+app.post('/generate-otp', async (req, res) => {
+    const email = req.body.Email;
+    const success = { "s": 'OTP sent successfully' };
+    const failure = { "f": "Error sending OTP" };
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    console.log("Generated OTP: ", otp);
+    var k1 = await Todo.findOne({ Email: req.body.Email });
+    console.log(k1);
+    if(k1==null){
+        try {
+            await OTP.create({ email, otp });
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'ryashraj8218@gmail.com',
+                    pass: 'tdij bgpf vrxl alue'
+                }
+            });
+            await transporter.sendMail({
+                from: 'ryashraj8218@gmail.com',
+                to: email,
+                subject: 'OTP Verification',
+                text: `Your OTP for verification is: ${otp}`
+            });
+            res.status(200).json(success);
+        } 
+        catch (error) {
+            console.log("Error occurred:", error); // Log any errors
+            res.status(500).json(failure);
         }
-        else{
-            response_value = {
-                "reply" : "false",
-                "to_be_displayed" : "Please enter another email for registering"
+    }
+    else{
+        res.json("Please enter another mail for registration because your currently entered mail is beign already regsitered")
+    }   
+});
+app.post('/verify-otp', async (req, res) => {
+    const email = req.body.Email; 
+    const otp = req.body.Otp; 
+    console.log('Email:', email);
+    console.log('OTP:', otp);
+    try {
+        const otpRecord = await OTP.findOne({ email: email, otp: otp });
+        if (otpRecord) {
+            const s = 'OTP verified successfully'
+            try {
+                console.log(req.body)
+                var complete_info_singnup = {
+                    "Name" : req.body.Name,
+                    "Age" : req.body.Age ,
+                    "Email" : req.body.Email,
+                    "Password" : req.body.Password,
+                  }
+                const todo = new Todo(complete_info_singnup)
+                console.log(todo)
+                var response_value = {}
+                var k1 = await Todo.findOne({ Email: req.body.Email });
+                if (k1 == null) {
+                    await todo.save()
+                    response_value = {
+                        "reply": "true",
+                        "to_be_displayed": "Siggned up successfully",
+                        "success" : s
+                    }
+                }
+                else {
+                    response_value = {
+                        "reply": "false",
+                        "to_be_displayed": "Please enter another email for registering",
+                        "success" : s
+                    }
+                }
             }
-        }    
-    } 
-    catch (error) {
-        console.log(error)
+            catch (error) {
+                console.log(error)
+            }
+            finally {
+                res.json(response_value)
+            }
+        } else {
+            res.json('Invalid OTP');
+        }
+    } catch (error) {
+        console.error('Error verifying OTP:', error);
+        res.status(500).json('Error verifying OTP');
     }
-    finally{
-        res.json(response_value)
-    }
-   
-})
+});
 app.get("/", function (req, res) {
     res.send("hello")
 })
--- Frontend/my_first_react_app/src/App.js
+++ Frontend/my_first_react_app/src/App.js
@@ -1,11 +1,11 @@
 
 import React from "react";
-
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./screens/Home";
 import SignIn from "./screens/SignIn";
 import Signup from "./screens/Signup";
 import Result from "./screens/Result";
+import Verification from "./screens/Verification";
 function App(){
   return (
     <div>
@@ -16,6 +16,7 @@ function App(){
             <Route path='/signin' element={<SignIn/>}/>
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/result' element={<Result/>}/>
+            <Route path='/verification' element={<Verification/>}/>
           </Routes>
         </BrowserRouter>
       </center>
